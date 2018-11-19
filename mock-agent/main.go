c4888231ae4602b226b433b14df84e7333804c07
Abdul Dakkak <abduld@wolfram.com>
2018-11-19 10:42:47 -0600

    update

--- mock-agent/main.go
+++ mock-agent/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	cmd "github.com/rai-project/dlframework/framework/cmd/server"
+	"github.com/rai-project/mock-predictor"
+	_ "github.com/rai-project/mock-predictor/predict"
+	"github.com/rai-project/tracer"
+)
+
+func main() {
+
+	rootCmd, err := cmd.NewRootCommand(tensorrt.Register, tensorrt.FrameworkManifest)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	defer tracer.Close()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
